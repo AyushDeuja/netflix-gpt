0963dddf917737737326b7f5f336805f797c6c72
AyushDeuja <ayushdeuja11@gmail.com>
2025-02-24 17:35:36 +0545

    saf

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,22 @@
+import Header from "./Header";
 
 const Login = () => {
   return (
     <div>
-      Login
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/NP-en-20250217-TRIFECTA-perspective_76dcb6f9-24a4-4224-8132-cb79a5094f75_large.jpg"
+          alt="Body-Image"
+        />
+      </div>
+      <form className="w-3/12 absolute  bg-black p-12 my-36 mx-auto right-0 left-0" >
+        <input type="text" placeholder="Email Address" className="p-2 m-2"/>
+        <input type="password" placeholder="Password" className="p-2 m-2"/>
+        <button className="p-4 m-4">Sign In</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
